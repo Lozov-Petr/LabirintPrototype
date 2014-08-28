77780802de087b02c78722ddaf17611dfa7d2cf0
Лозов Пётр <not_kill@mail.ru>
2014-08-28 22:03:16 +0400

    Added class "Inventory"

---
+++ LabPrototype/Inventory/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabPrototype
+{
+    class Inventory
+    {
+        List<Item> items = new List<Item>();
+    }
+}
